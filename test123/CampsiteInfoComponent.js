d85ec3106022fb35bf94a0e9f6e193284597b851
Adnane Ahmat adoum <adnaneahmatadoum@Adnanes-MacBook-Pro.local>
2021-12-26 21:21:14 -0500

    I will

---
+++ test123/CampsiteInfoComponent.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class CampsiteInfo extends Component{
+    render(){
+        if(this.props.campsite){
+           return(
+            <div className ="row">
+            
+
+            </div>
+           );
+
+        }
+        return <div></div>;
+    }
+
+}
+export default CampsiteInfo;
